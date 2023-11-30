4daa6e45de2de81660a930a0a092490c1373d899
rajmohanb82 <91312695+rajmohanb82@users.noreply.github.com>
2023-11-30 08:32:18 +0530

    creds removed from code and aded as environment in space lift -  commit

--- main.tf
+++ main.tf
@@ -9,8 +9,6 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "AKIA4U7KR5QRHK6VHTBC"
-  secret_key = "qp4DbSCPAY2GrJSx8vDN0eQdwRfuQ+Xufv8dX5bz"
   region     = "us-east-1" # Set your desired AWS region
 }
 
