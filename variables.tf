0cb9516289f65580e5564af4bc07424af37f17e3
rajmohanb82 <91312695+rajmohanb82@users.noreply.github.com>
2023-11-30 09:25:03 +0530

    sting quotes removed -  commit

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "instance_name" {
     description = "Value of the Name tag for the EC2 Instance"
-    type = "string"
+    type = string
  }
  variable "vpc_name" {
     description = "Value of the Name tag for the VPC"
-    type = "string"
+    type = string
  }
