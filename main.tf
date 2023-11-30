0b8f9d367f4ef9186b0cec4f9dec3d51a6b19df8
rajmohanb82 <91312695+rajmohanb82@users.noreply.github.com>
2023-11-30 06:16:53 +0530

    first commmit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+      }
+    }
+    required_version = ">=1.2.0"
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+
+    ami = "ami-0fc5d935ebf8bc3bc"
+    instance_type = "t3.medium"
+    
+    tags = {
+        Name = "webapp"
+        }
+    }
+
---
+++ output.tf
@@ -0,0 +1,9 @@
+output "instance_id" {
+    description = "ID of the EC2 Instance"
+    value = aws_intance.app_server.id
+}
+
+output "instance_pub;ic_ip" {
+    description = "Public IP Address of EC2 Instance"
+    value = aws_intance.app_server.public_ip
+}
