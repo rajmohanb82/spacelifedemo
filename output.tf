44c954d39a26b2c2b4542c2a57485e9b2a1459f7
rajmohanb82 <91312695+rajmohanb82@users.noreply.github.com>
2023-11-30 07:33:44 +0530

    forth commit

--- output.tf
+++ output.tf
@@ -1,6 +1,9 @@
 output "instance_id" {
     description = "ID of the EC2 Instance"
-    value = aws_intance.app_server.id
+    value = aws_instance.app_server.id
 }
-
+output "instance_public_ip" {
+    description = "Public IP Address of EC2 Instance"
+    value = aws_instance.app_public_ip
+}
 # random comment
